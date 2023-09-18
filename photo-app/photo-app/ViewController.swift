98c631ac368edaf5e828a7b678d0fda7ca16bf84
Marco Agizza <m.agizza@reply.it>
2023-09-18 12:57:46 +0200

    Project Initialization

---
+++ photo-app/photo-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  photo-app
+//
+//  Created by Marco Agizza on 18/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
